f66eadf8ce68e202cae611372f339c27738f1c7f
marya3mouri <m.3mouri@gmail.com>
2026-07-12 16:37:35 +0300

    setup server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server is running on port 3000");
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}   
+)
+ 

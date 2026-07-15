ccde552dc425b9bd9c036bd4934934207010991f
marya3mouri <m.3mouri@gmail.com>
2026-07-15 17:13:39 +0300

    add the first route

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+
+app.get('/', (req, res) => { 
+  res.status(200).send('API is running...')
+ })
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}   
-)
- 
+}); 
